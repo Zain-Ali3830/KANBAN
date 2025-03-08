f0d88e4ab9189fecc8e189cca1521801dcd1257b
Zain Ali <zainidrees3830@gmail.com>
2025-03-08 02:08:54 -0800

    change file name

--- kanban.js
+++ kanban.js
@@ -1,63 +1,108 @@
-let addBtn=document.getElementById('add');
-let addTaskBtn=document.getElementById('addTask')
-let modale=document.getElementById('modale')
-let option=document.getElementById('select')
-let todolist=document.getElementById('todoList')
-let inProgress=document.getElementById('inProgress')
-let Done=document.getElementById('done')
-let toDoArray=[];
-addBtn.addEventListener('click',()=>{
+let addBtn = document.getElementById('add');
+let addTaskBtn = document.getElementById('addTask');
+let modale = document.getElementById('modale');
+let option = document.getElementById('select');
+let todolist = document.getElementById('todoList');
+let inProgress = document.getElementById('inProgress');
+let Done = document.getElementById('done');
+let mainArray = [];
+let todo, inP, don;
+
+addBtn.addEventListener('click', () => {
     modale.classList.add('show');
-})
+});
 
-//  Adding values in ToDo
-addTaskBtn.addEventListener('click',()=>{
-    let title=document.getElementById('title');
-    let desc=document.getElementById('desc')
-    
+// Adding values in ToDo
+addTaskBtn.addEventListener('click', () => {
+    let title = document.getElementById('title');
+    let desc = document.getElementById('desc');
+
+    // Creating Object
+    let obj = {
+        tit: title.value,
+        des: desc.value,
+    };
+    console.log(obj);
+    mainArray.unshift(obj);
+    console.log(mainArray)
+
+
     // Checking condition
-    if(option.selectedIndex===0){
-        alert("Please Select the option")
-        return
+    if (option.selectedIndex === 0) {
+        alert("Please Select the option");
+        return;
     }
 
-    //  Adding value in To do
-    if(option.value==="To Do"){
-       let todo=document.createElement('li')
-       todo.classList.add('todo')
-       todo.innerHTML=`<p>${title.value}</p><p> ${desc.value}</p> `
-       title.value=""
-       desc.value="";
-       option.selectedIndex=0;
-       toDoArray.push(todo);
-       console.log(toDoArray)
-       todolist.append(todo)
-       modale.classList.remove('show')
+                    // Create Element
+    function createTaskElement(title, desc, type) {
+        let task = document.createElement('li');
+        task.draggable = true;
+        task.classList.add('todo');
+        task.innerHTML = `<p>${title}</p><p>${desc}</p>`;
+        task.setAttribute('data-type', type);
+        task.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('text/plain', task.outerHTML);
+            task.classList.add('dragging');
+        });
+        task.addEventListener('dragend', () => {
+            task.classList.remove('dragging');
+        });
+        return task;
+    }
+    let taskElement = createTaskElement(title.value, desc.value, option.value);
+
+                    // Adding  task to the selected category
+    if (option.value === "To Do") {
+        todolist.appendChild(taskElement);
+    } else if (option.value === "In Progress") {
+        inProgress.appendChild(taskElement);
+    } else if (option.value === "Done") {
+        Done.appendChild(taskElement);
     }
 
-// Adding value in In Progress
-     if(option.value==="In Progress"){
-        let inP =document.createElement('li')
-        inP.innerHTML=`<p>${title.value}</p><p> ${desc.value}</p> `
-        inP.classList.add('todo');
-        title.value=""
-        desc.value="";
-        option.selectedIndex=0;
-        inProgress.append(inP)
-        modale.classList.remove('show')
-     }
+    modale.classList.remove('show');
 
-     // Adding value in Done
-     if(option.value==="Done"){
-        let don =document.createElement('li')
-        don.innerHTML=`<p>${title.value}</p><p> ${desc.value}</p> `
-        don.classList.add('todo')
-        title.value=""
-        desc.value="";
-        option.selectedIndex=0;
-        Done.append(don)
-        modale.classList.remove('show')
-     }
-})
+    // Reset input fields
+    title.value = "";
+    desc.value = "";
+    option.selectedIndex = 0;
+});
 
+// Search Functionality
+let search = document.getElementById('search');
+search.addEventListener('keyup', () => {
+    let searchValue = search.value.toLowerCase();
+    document.querySelectorAll('li').forEach(item => {
+        let itemTitle = item.textContent.toLowerCase();
+        item.style.display = itemTitle.includes(searchValue) ? '' : 'none';
+    });
+});
 
+// Drag and Drop Functionality
+document.querySelectorAll('.list').forEach(list => {
+    list.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    list.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let taskHTML = e.dataTransfer.getData('text/plain');
+        list.innerHTML += taskHTML;
+        document.querySelectorAll('.dragging').forEach(task => task.remove());
+        addDragEvents();
+    });
+});
+
+// Function to add drag events after dropping
+function addDragEvents() {
+    document.querySelectorAll('li').forEach(task => {
+        task.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('text/plain', task.outerHTML);
+            task.classList.add('dragging');
+        });
+        task.addEventListener('dragend', () => {
+            task.classList.remove('dragging');
+        });
+    });
+}
+
+addDragEvents();
